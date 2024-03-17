e7cd15216b852fce4903e4fdf6c126359810f762
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-03-17 14:10:48 -0700

    pages created

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,39 @@ import "./App.css";
 // data
 import { useSearchForMovies } from "./utils/fetchMovies.js";
 
+// pages
+// import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import Actor from "./pages/Actor";
+import Staff from "./pages/Staff";
+import Studio from "./pages/Studio";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Root />} caseSensitive={true}>
           <Route path="/" element={<Home />} caseSensitive={true} />
+          <Route
+            path="/movie/:movie-name"
+            element={<Home />}
+            caseSensitive={true}
+          />
+          <Route
+            path="/actor/:actor-name"
+            element={<Home />}
+            caseSensitive={true}
+          />
+          <Route
+            path="/staff/:staff-name"
+            element={<Home />}
+            caseSensitive={true}
+          />
+          <Route
+            path="/studio/:studio-name"
+            element={<Home />}
+            caseSensitive={true}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
@@ -30,21 +57,41 @@ function Root(props) {
   const { children } = props;
 
   return (
-    <div className="text">
-      <nav>
-        <header>
+    <div className="text bg-black min-h-screen w-full">
+      {/* Header */}
+      <nav className="h-[5rem] bg-black flex justify-between mx-[5rem]">
+        <header className="text-lheader font-bold">
           <h1>Movie Search</h1>
         </header>
-        <ul>
+        <ul className="flex gap-5">
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink className="navLinkStyle" to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink className="navLinkStyle" to="/movie">Movie</NavLink>
+          </li>
+          <li>
+            <NavLink className="navLinkStyle" to="/actor">Actor</NavLink>
+          </li>
+          <li>
+            <NavLink className="navLinkStyle" to="/staff">Staff</NavLink>
+          </li>
+          <li>
+            <NavLink className="navLinkStyle" to="/studio">Studio</NavLink>
           </li>
         </ul>
       </nav>
 
-      <main>{children || <Outlet />}</main>
+      {/* Main content */}
+      <div
+        className="bg h-full flex-grow on_desktop:px-20 on_mobile:px-[3vw] pb-10"
+        style={{ minHeight: "calc(100vh - 15rem)" }}
+      >
+        <main>{children || <Outlet />}</main>
+      </div>
 
-      <footer>
+      {/* Footer */}
+      <footer className="h-[10rem]">
         <p>Movie Search</p>
         <p>Grant Conklin | Rylan Harwood | Tyler Greenwood</p>
       </footer>
---
+++ src/pages/Studio.jsx
@@ -0,0 +1,5 @@
+export default function Studio() {
+  return (
+    <div>Studio</div>
+  )
+}
