51f97730ae660c02105707f6b80c09b8c6f952e2
pdxgrantc <47283493+pdxgrantc@users.noreply.github.com>
2024-03-20 00:40:32 -0700

    re oraganized actor

--- src/pages/Actor.jsx
+++ src/pages/Actor.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import PropTypes from "prop-types";
 
 // data
-import { searchForActor } from "../utils/ActorApiInterface";
+import {
+  searchForActor,
+  searchForActorCredits,
+} from "../utils/ActorApiInterface";
 
 export default function Actor() {
   // pull the movie id from the url
   const { actorID } = useParams();
   // create state to hold the movie details
   const [actorDetails, setActorDetails] = useState(null);
+  const [actorCredits, setActorCredits] = useState(null);
 
   useEffect(() => {
     if (actorID) {
@@ -23,51 +28,98 @@ export default function Actor() {
     }
   }, [actorID]);
 
-  console.log(actorDetails);
+  useEffect(() => {
+    if (actorID) {
+      searchForActorCredits(actorID).then((response) => {
+        if (response.total_results !== 0) {
+          console.log(response);
+          setActorCredits(response);
+        } else {
+          setActorCredits([]);
+        }
+      });
+    }
+  }, [actorID]);
+
+  console.log(actorCredits);
 
   return (
     <>
-      <div className="flex gap-10">
-        <div style={{ width: "50%" }}>
-          <h1>{actorDetails?.name}</h1>
-          <h2>From: {actorDetails?.known_for_department}</h2>
-          <h2>From: {actorDetails?.place_of_birth}</h2>
-          {actorDetails?.parent_company !== "" && (
-            <h2>{actorDetails?.parent_company}</h2>
-          )}
-          {actorDetails?.also_known_as.length !== 0 && (
-            <table>
-              <tbody>
-                {actorDetails?.also_known_as.map((name, index) => (
-                  <tr key={index}>
-                    <td className="pr-4">{index === 0 ? "Also Known As:" : ""}</td>
-                    <td>{name}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
-          <div>
-            <h2>Biography:</h2>
-            {actorDetails?.biography !== "" && (
-              <h3 className="">{actorDetails?.biography}</h3>
-            )}
-          </div>
-        </div>
-        <div className="w-1/2 inline-block h-fit bg-logoBG px-10 py-8 rounded-image">
-          <a href={actorDetails?.homepage} target="_blank" rel="noreferrer">
+      <div className="grid grid-cols-2 gap-10">
+        <div>
+          <div className="inline-block h-fit bg-logoBG px-10 py-8 rounded-image">
             <img
               className="h-fit"
               src={`https://image.tmdb.org/t/p/w500${actorDetails?.profile_path}`}
               alt={actorDetails?.name}
-              style={{ width: "100%" }}
             />
-          </a>
+          </div>
+          <div>
+            <h1>{actorDetails?.name}</h1>
+            <h2>From: {actorDetails?.known_for_department}</h2>
+            <h2>From: {actorDetails?.place_of_birth}</h2>
+            {actorDetails?.parent_company !== "" && (
+              <h2>{actorDetails?.parent_company}</h2>
+            )}
+            {actorDetails?.also_known_as.length !== 0 && (
+              <table>
+                <tbody>
+                  {actorDetails?.also_known_as.map((name, index) => (
+                    <tr key={index}>
+                      <td className="pr-4">
+                        {index === 0 ? "Also Known As:" : ""}
+                      </td>
+                      <td>{name}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+            <div>
+              <h2>Biography:</h2>
+              {actorDetails?.biography !== "" && (
+                <h3 className="">{actorDetails?.biography}</h3>
+              )}
+            </div>
+          </div>
         </div>
+        <ActorMovieCredits credits={actorCredits?.cast} />
       </div>
     </>
   );
 }
+
+function ActorMovieCredits({ credits }) {
+  return (
+    <div>
+      <h2>Credits</h2>
+      {credits?.length === 0 ? (
+        <h3>No Credits Found</h3>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Movie</th>
+              <th>Character</th>
+            </tr>
+          </thead>
+          <tbody>
+            {credits?.map((movie) => (
+              <tr key={movie.id}>
+                <td>{movie.title}</td>
+                <td>{movie.character}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+ActorMovieCredits.propTypes = {
+  credits: PropTypes.array,
+};
 
 /*
 import { useState, useEffect } from "react";
--- src/utils/ActorApiInterface.js
+++ src/utils/ActorApiInterface.js
@@ -14,5 +14,21 @@ const searchForActor = async (query) => {
     }
 }
 
+const searchForActorCredits = async (query) => {
+    if (query) {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/person/${query}/movie_credits?language=en-US`,
+            {
+                headers: {
+                    "Authorization": `Bearer ${import.meta.env.VITE_KEY}`
+                }
+            }
+        );
+        return response.json();
+    } else {
+        return null;
+    }
+}
+
 // Studio API Interface Functions
-export { searchForActor };
+export { searchForActor, searchForActorCredits };
